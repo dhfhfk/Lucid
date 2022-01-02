b9f1a9f69935c4e86491f9c547027a997a3276f3
dhfhfk <dhfhfk1203@gmail.com>
2022-01-02 16:59:14 +0900

    Remove copy URL link button

--- assets/js/post.js
+++ assets/js/post.js
@@ -262,15 +262,3 @@
     },
     [[23, 0, 1]],
 ]);
-
-function clip(){
-	var url = '';
-	var textarea = document.createElement("textarea");
-	document.body.appendChild(textarea);
-	url = window.document.location.href;
-	textarea.value = url;
-	textarea.select();
-	document.execCommand("copy");
-	document.body.removeChild(textarea);
-	alert("URL이 복사되었습니다.")
-}
